5097e48a9956ce75b2e447ba52d3277c52460e44
ryanchou-dev <ryanchou111@gmail.com>
2023-04-09 07:14:12 -0700

    Card Generation

---
+++ components/Card.jsx
@@ -0,0 +1,179 @@
+import { motion, useAnimation } from "framer-motion";
+import { useState, useRef } from "react";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import { useReactToPrint } from "react-to-print";
+
+const cardVariants = {
+  closed: {
+    transform: "rotateX(10deg) translateY(100%)",
+  },
+  open: {
+    transform: "rotateX(170deg) translateY(100%)",
+  },
+};
+
+const textVariants = {
+  closed: {
+    opacity: 1,
+  },
+  open: {
+    opacity: 0,
+  },
+};
+
+const Card = ({ message = "", preview, qrCode }) => {
+  const [title, setTitle] = useState("dear john,");
+  const controls = useAnimation();
+  const [open, setOpen] = useState(false);
+  const [letter, setLetter] = useState("hi john. how are you?");
+  const componentRef = useRef();
+
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleLetterChange = (e) => {
+    setLetter(e.target.value);
+  };
+  return (
+    <div className="py-16 px-8 overflhow-x-hidden">
+      <button onClick={handlePrint}>Print this out!</button>
+      <div
+        className="relative flex flex-col items-center w-full"
+        style={{ perspective: 1000 }}
+      >
+        <div
+          className="overflow-visible hidden print:block  w-full h-[30rem] bg-white"
+          ref={componentRef}
+        >
+          <div className="border-slate-500 border-4 w-full h-1/2 flex flex-col items-center justify-center">
+            <h1 className="rotate-180 text-center font-bold font-serif text-2xl">
+              {title}
+            </h1>
+          </div>
+
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+
+          <div className="h-3/4 w-screen ">
+            <div className="w-1/6 ml-2 mt-24 ">
+              <div className="flex items-center fixed left-1 justify-center">
+                <p className=" text-sm text-center">Scan me! </p>
+                <img
+                  src={qrCode}
+                  className={"border-2 text-center border-black roudned-lg"}
+                />
+              </div>
+              <div className="h-1/2 mt-24 ">
+                <div className="w-1/2 -mt-4 right-12 fixed">
+                  <img src={preview} className="-rotate-90" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        Cover:{" "}
+        <input
+          type="text"
+          onChange={handleTitleChange}
+          className="z-50"
+          defaultValue={title}
+        />
+        <div className="absolute w-full sm:w-2/3 max-w-2xl aspect-video flex flex-col items-center justify-center">
+          <motion.div
+            variants={textVariants}
+            initial="closed"
+            animate={controls}
+            transition={{
+              duration: 2,
+            }}
+            className="absolute bottom-1 flex flex-col items-center"
+          ></motion.div>
+        </div>
+        <motion.div
+          onTap={() => {
+            if (open) {
+              setOpen(false);
+              controls.start("closed");
+            } else {
+              setOpen(true);
+              controls.start("open");
+            }
+          }}
+          variants={cardVariants}
+          initial="closed"
+          animate={controls}
+          transition={{
+            duration: 3,
+          }}
+          className="cursor-pointer relative w-full sm:w-2/3 max-w-2xl aspect-video z-10"
+          style={{
+            transformOrigin: "center bottom",
+            transform: "translateY(100%)",
+            transformStyle: "preserve-3d",
+          }}
+        >
+          <div className="absolute inset-0">
+            <div
+              className="absolute inset-0 bg-white"
+              style={{
+                transform: "rotateX(180deg)",
+              }}
+            >
+              <div className="flex justify-center items-center">
+                <div className="w-1/4">
+                  <p className="mt-12 text-sm text-center">Scan me! </p>
+                  <img
+                    src={qrCode}
+                    className={"border-2 border-black roudned-lg"}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="absolute inset-0 bg-white"
+            style={{ backfaceVisibility: "hidden" }}
+          >
+            <div className="overflow-hidden relative w-full h-full p-2 sm:p-4">
+              <div className="border-slate-500 border-4 w-full h-full flex flex-col items-center justify-center">
+                <h1 className="text-center font-bold font-serif  sm:text-xl ">
+                  {title} {/* make this editable */}
+                </h1>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+        <div
+          className="overflow-auto w-full sm:w-2/3 max-w-2xl aspect-video will-change-transform bg-white"
+          style={{
+            transformOrigin: "center top",
+            transform: "rotateX(10deg)",
+          }}
+        >
+          <div className="flex justify-center items-center max-w-none whitespace-pre-wrap p-2 sm:p-4 pt-0 text-xs sm:text-base w-full h-full prose">
+            <div className="w-2/4 h-full -mt-4 ">
+              <img src={preview} className="-rotate-90" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
